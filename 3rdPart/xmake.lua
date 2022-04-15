67ca3844cee69d10f9b3db47317f3af48234f716
45degree <1098902652@qq.com>
2022-04-15 17:22:21 +0800

    update 3rd deps

---
+++ 3rdPart/xmake.lua
@@ -0,0 +1,53 @@
+---@diagnostic disable: undefined-global
+
+add_rules("mode.debug", "mode.release")
+
+add_requires("glfw 3.3.6")
+
+package("uchardet")
+    add_deps("cmake")
+    set_sourcedir(path.join("$(projectdir)", "3rdPart", "uchardet"))
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        table.insert(configs, "-DBUILD_BINARY=" .. "OFF")
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("uchardet_get_charset", {includes = "uchardet/uchardet.h"}))
+    end)
+package_end()
+
+target("Imgui")
+    set_kind("static")
+    set_languages("c11", "cxx11")
+    add_includedirs("$(projectdir)/3rdPart/imgui", { public = true })
+    add_includedirs("$(projectdir)/3rdPart/imgui/backends", {public = true})
+    add_files("$(projectdir)/3rdPart/imgui/*.cpp")
+    add_files("$(projectdir)/3rdPart/imgui/backends/imgui_impl_glfw.cpp")
+    add_files("$(projectdir)/3rdPart/imgui/backends/imgui_impl_opengl3.cpp")
+
+    add_packages("glfw")
+target_end()
+
+target("ImGuiFileDialog")
+    set_kind("static")
+    set_languages("c11", "cxx17")
+    add_deps("Imgui")
+
+    add_defines("USE_STD_FILESYSTEM")
+
+    add_files("$(projectdir)/3rdPart/ImGuiFileDialog/**.cpp")
+    add_includedirs("$(projectdir)/3rdPart/ImGuiFileDialog", { public = true })
+target_end();
+
+target("ImGuizmo")
+    set_kind("static")
+    set_languages("c11", "cxx11")
+    add_deps("Imgui")
+
+    add_includedirs("$(projectdir)/3rdPart/ImGuizmo", { public = true })
+    add_files("$(projectdir)/3rdPart/ImGuizmo/*.cpp")
+target_end()
--- xmake.lua
+++ xmake.lua
@@ -2,23 +2,9 @@
 
 add_rules("mode.debug", "mode.release")
 
-package("uchardet")
-    add_deps("cmake")
-    set_sourcedir(path.join("$(projectdir)", "3rdPart", "uchardet"))
-    on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DBUILD_BINARY=" .. "OFF")
-        import("package.tools.cmake").install(package, configs)
-    end)
+includes("3rdPart/")
 
-    on_test(function (package)
-        assert(package:has_cfuncs("uchardet_get_charset", {includes = "uchardet/uchardet.h"}))
-    end)
-package_end()
-
-add_requires("glfw 3.3.5")
+add_requires("glfw 3.3.6")
 add_requires("glew 2.2.0")
 add_requires("glm 0.9.9+8")
 add_requires("glog v0.5.0")
@@ -29,39 +15,6 @@ add_requires("toml++ v3.0.0")
 add_requires("libiconv 1.16")
 add_requires("gtest 1.11.0")
 add_requires("uchardet")
-
-target("Imgui")
-    set_kind("static")
-    set_languages("c11", "cxx11")
-    add_includedirs("$(projectdir)/3rdPart/imgui", { public = true })
-    add_includedirs("$(projectdir)/3rdPart/", { public = true })
-    add_includedirs("$(projectdir)/3rdPart/imgui/backends", {public = true})
-    add_files("$(projectdir)/3rdPart/imgui/*.cpp")
-    add_files("$(projectdir)/3rdPart/imgui/backends/imgui_impl_glfw.cpp")
-    add_files("$(projectdir)/3rdPart/imgui/backends/imgui_impl_opengl3.cpp")
-
-    add_packages("glfw")
-target_end()
-
-target("ImGuiFileDialog")
-    set_kind("static")
-    set_languages("c11", "cxx17")
-    add_deps("Imgui")
-
-    add_defines("USE_STD_FILESYSTEM")
-
-    add_files("$(projectdir)/3rdPart/ImGuiFileDialog/**.cpp")
-    add_includedirs("$(projectdir)/3rdPart/ImGuiFileDialog", { public = true })
-target_end();
-
-target("ImGuizmo")
-    set_kind("static")
-    set_languages("c11", "cxx11")
-    add_deps("Imgui")
-
-    add_includedirs("$(projectdir)/3rdPart/ImGuizmo", { public = true })
-    add_files("$(projectdir)/3rdPart/ImGuizmo/*.cpp")
-target_end()
 
 includes("src/RHI")
 includes("src/Tool")
