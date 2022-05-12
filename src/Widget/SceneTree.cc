553d44f57ece04c515e20581d697877185cd9765
45degree <1098902652@qq.com>
2022-05-12 12:16:12 +0800

    (unfinsh) change widget

--- src/Widget/SceneTree.cc
+++ src/Widget/SceneTree.cc
@@ -24,10 +24,29 @@ void SceneTreeWidget::DrawNode(const SceneNode* node) {
       }
     }
 
+    if (ImGui::IsMouseClicked(ImGuiMouseButton_Right)) {
+      ImGui::OpenPopup("scenePopup");
+    }
+
+    if (ImGui::BeginPopup("scenePopup")) {
+      ImGui::Text("%s", node->GetSceneNodeName());
+      if (ImGui::MenuItem("Add Model")) {
+        m_modeladdDialog->Open();
+        m_selectedNode = const_cast<SceneNode*>(node);
+      }
+      if (node == m_scene->GetRootSceneNode()) {
+        if (ImGui::MenuItem("Change SkyBox")) {
+        }
+      }
+
+      ImGui::EndPopup();
+    }
+
     auto subNodes = node->GetSubSceneNodes();
     for (const auto* subNode : subNodes) {
       DrawNode(subNode);
     }
+
     ImGui::TreePop();
   }
 }
@@ -37,6 +56,23 @@ void SceneTreeWidget::Draw() {
 
   auto* sceneRoot = m_scene->GetRootSceneNode();
   DrawNode(sceneRoot);
+
+  m_modeladdDialog->Draw();
+}
+
+SceneTreeWidget::SceneTreeWidget() : Widget("SceneTree") {
+  FileDialogCrateInfo info{
+      "TextureOpenDialog",
+      "Open a texture",
+      "model file (*.off *.obj *.ply *.pmx){.off,.obj,.ply,.pmx}",
+  };
+  m_modeladdDialog = std::make_unique<FileDialog>(info);
+
+  m_modeladdDialog->SelectCallback([&](const char* filePathName, const char* fileName) {
+    // auto scene = Scene::CreateSceneFromFile(filePathName, m_resourceManager);
+    // auto renderLayer = m_window->GetRenderLayer();
+    // renderLayer->SetSecne(std::move(scene));
+  });
 }
 
 }  // namespace Marbas
--- src/Widget/SceneTree.hpp
+++ src/Widget/SceneTree.hpp
@@ -2,13 +2,14 @@
 #define MARBAS_WIDGET_SCENE_TREE
 
 #include "Core/Scene.hpp"
+#include "Widget/FileDialog.hpp"
 #include "Widget/Widget.hpp"
 
 namespace Marbas {
 
 class SceneTreeWidget : public Widget {
  public:
-  explicit SceneTreeWidget() : Widget("SceneTree") {}
+  explicit SceneTreeWidget();
   ~SceneTreeWidget() override = default;
 
  public:
@@ -20,7 +21,9 @@ class SceneTreeWidget : public Widget {
   void DrawNode(const SceneNode* node);
 
  private:
+  SceneNode* m_selectedNode = nullptr;
   Vector<IChangeMeshAble*> m_selectMeshWidgets;
+  std::unique_ptr<FileDialog> m_modeladdDialog = nullptr;
 };
 
 }  // namespace Marbas
