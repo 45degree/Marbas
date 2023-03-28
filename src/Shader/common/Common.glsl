489e81366c3e7286dbf13b0dbf4d6d7267096a4f
45degree <1098902652@qq.com>
2023-03-28 16:32:51 +0800

    (unfinish) update

---
+++ src/Shader/common/Common.glsl
@@ -0,0 +1,56 @@
+#ifndef MARBAS_COMMON_COMMON_GLSL
+#define MARBAS_COMMON_COMMON_GLSL
+
+#include "ConstValue.glsl"
+
+/**
+ * @see https://blog.csdn.net/silangquan/article/details/17464821
+ */
+vec3
+RayIntersectSphere(const vec3 spherePoint, const float sphereRadius, const vec3 rayPoint, const vec3 rayDir) {
+  vec3 l = spherePoint - rayPoint;
+  float s = dot(l, rayDir);
+  float l_2 = dot(l, l);
+  float r_2 = sphereRadius * sphereRadius;
+  if(s < 0 &&  l_2 > r_2) return vec3(-1, 0, 0);
+
+  float m_2 = l_2 - s * s;
+  if(m_2 > r_2) return vec3(-1, 0, 0);
+
+  float q = sqrt(r_2 - m_2);
+  return vec3(1, s - q, s + q);
+}
+
+/**
+ * @berif convert a direction to a Equirectangular UV
+ * @see https://learnopengl.com/PBR/IBL/Diffuse-irradiance
+ */
+vec2
+DirToCubemapUV2D(vec3 dir) {
+  const vec2 invAtan = vec2(1 / (2 * PI), 1 / PI);
+  dir = normalize(dir);
+
+  vec2 uv = vec2(atan(dir.z, dir.x), asin(dir.y));
+  uv *= invAtan;
+  uv += 0.5;
+
+  return uv;
+}
+
+/**
+ * @berif convert a Equirectangular UV to direction, the inverse operator of DirToCubemapUV2D.
+ * @see DirToCubemapUV2D
+ */
+vec3
+CubemapUV2DToDir(vec2 uv) {
+  float theta = (1.0 - uv.y) * PI;
+  float phi = (uv.x * 2 - 1) * PI;
+
+  float x = sin(theta) * cos(phi);
+  float z = sin(theta) * sin(phi);
+  float y = cos(theta);
+
+  return vec3(x, y, z);
+}
+
+#endif
---
+++ src/Shader/common/ConstValue.glsl
@@ -0,0 +1,6 @@
+#ifndef CONST_VALUE_GLSL
+#define CONST_VALUE_GLSL
+
+#define PI 3.14159265359
+
+#endif
