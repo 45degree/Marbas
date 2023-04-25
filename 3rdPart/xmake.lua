3345374882a4e968848342305dd60e73c93c53bf
45degree <1098902652@qq.com>
2023-04-25 16:14:42 +0800

    update 3rdPart

--- 3rdPart/xmake.lua
+++ 3rdPart/xmake.lua
@@ -1,25 +1,26 @@
 ---@diagnostic disable: undefined-global
 
-add_rules("mode.debug", "mode.release")
+add_rules('mode.debug', 'mode.release')
 
-includes("./Marbas.RHI")
+includes('./Marbas.RHI')
+includes('./async_simple')
 
-target("ImGuizmo")
-    set_kind("static")
-    set_languages("c11", "cxx17")
-    add_deps("Marbas.RHI")
+target('ImGuizmo', function()
+  set_kind('static')
+  set_languages('c11', 'cxx17')
+  add_deps('Marbas.RHI')
 
-    add_includedirs(path.join(os.scriptdir(), "ImGuizmo"), { public = true })
-    add_files(path.join(os.scriptdir(), "ImGuizmo/*.cpp"))
-target_end()
+  add_includedirs(path.join(os.scriptdir(), 'ImGuizmo'), { public = true })
+  add_files(path.join(os.scriptdir(), 'ImGuizmo/*.cpp'))
+end)
 
-target("IconFontCppHeaders")
-    set_kind("headeronly")
+target('IconFontCppHeaders', function()
+  set_kind('headeronly')
 
-    add_includedirs(path.join(os.scriptdir(), "IconFontCppHeaders/"), { public = true })
-target_end()
+  add_includedirs(path.join(os.scriptdir(), 'IconFontCppHeaders/'), { public = true })
+end)
 
-target("nameof")
-  set_kind("headeronly")
-  add_includedirs(path.join(os.scriptdir(), "nameof/include"), { public = true })
-target_end()
+target('nameof', function()
+  set_kind('headeronly')
+  add_includedirs(path.join(os.scriptdir(), 'nameof/include'), { public = true })
+end)
